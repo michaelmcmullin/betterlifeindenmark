9807d29964328296145648de6cd45efdf5ffe874
mrmcnerd <blueark@outlook.com>
2017-05-22 07:29:00 +0100

    Adds heading to the graph

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,6 @@
 var svg = dimple.newSvg("#chart", 1200, 600);
 d3.tsv("denmark.tsv", function (data) {
+    "use strict";
     var denmarkChart = new dimple.chart(svg, data);
     denmarkChart.setMargins(200, 30, 50, 50)
     var x = denmarkChart.addMeasureAxis("x", "Difference");
