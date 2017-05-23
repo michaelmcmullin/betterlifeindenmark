0c854af7722d8a26e503d22edcdc11598166a8bf
mrmcnerd <blueark@outlook.com>
2017-05-23 07:34:18 +0100

    Adds basic support for country selection
    This is quite buggy at the moment. It can only really select one country reliably. Also, countries that contain null values display incorrectly.

--- js/graph.js
+++ js/graph.js
@@ -1,14 +1,20 @@
 var svg = dimple.newSvg("#chart", 1200, 600);
+var countries = d3.select("#countries");
+
 d3.tsv("all_countries.tsv", function (data) {
     "use strict";
-    var denmarkChart = new dimple.chart(svg, data);
-    denmarkChart.setMargins(200, 30, 50, 50)
-    var x = denmarkChart.addMeasureAxis("x", "Denmark");
-    x.title = "Comparison with OECD Average";
-    x.overrideMin = -50;
-    x.overrideMax = 50;
-    var y = denmarkChart.addCategoryAxis("y", "Category");
-    y.addOrderRule("Order", true);
-    denmarkChart.addSeries(null, dimple.plot.bar);
-    denmarkChart.draw();
+    var countryChart = new dimple.chart(svg, data);
+    countryChart.setMargins(200, 30, 50, 50);
+
+    countries.on("change", function(d){
+        var country = d3.select(this).property("value");
+        var x = countryChart.addMeasureAxis("x", country);
+        x.title = "Comparison with OECD Average";
+        x.overrideMin = -50;
+        x.overrideMax = 50;
+        var y = countryChart.addCategoryAxis("y", "Category");
+        y.addOrderRule("Order", true);
+        countryChart.addSeries(null, dimple.plot.bar);
+        countryChart.draw();
+    });
 });
