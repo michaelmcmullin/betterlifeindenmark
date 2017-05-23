b40040b018d3436e1ea8ff6e039a38ed7caea49d
mrmcnerd <blueark@outlook.com>
2017-05-23 07:12:23 +0100

    Replaces data file with all counties

--- js/graph.js
+++ js/graph.js
@@ -1,14 +1,14 @@
 var svg = dimple.newSvg("#chart", 1200, 600);
-d3.tsv("denmark.tsv", function (data) {
+d3.tsv("all_countries.tsv", function (data) {
     "use strict";
     var denmarkChart = new dimple.chart(svg, data);
     denmarkChart.setMargins(200, 30, 50, 50)
-    var x = denmarkChart.addMeasureAxis("x", "Difference");
+    var x = denmarkChart.addMeasureAxis("x", "Denmark");
     x.title = "Comparison with OECD Average";
     x.overrideMin = -50;
     x.overrideMax = 50;
     var y = denmarkChart.addCategoryAxis("y", "Category");
-    y.addOrderRule("Difference");
+    y.addOrderRule("Order", true);
     denmarkChart.addSeries(null, dimple.plot.bar);
     denmarkChart.draw();
 });
