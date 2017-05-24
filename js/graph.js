9a52f6a23102813886b0f4a8b993e37981caf883
mrmcnerd <blueark@outlook.com>
2017-05-24 06:58:00 +0100

    Allows chart to update for different countries
    Even discovered something new: the draw() function can take a time interval parameter, and will animate to the updated data. Cool!

--- js/graph.js
+++ js/graph.js
@@ -1,20 +1,23 @@
 var svg = dimple.newSvg("#chart", 1200, 600);
 var countries = d3.select("#countries");
+var country = "Denmark";
 
 d3.tsv("all_countries.tsv", function (data) {
     "use strict";
     var countryChart = new dimple.chart(svg, data);
     countryChart.setMargins(200, 30, 50, 50);
+    var x = countryChart.addMeasureAxis("x", country);
+    x.title = "Comparison with OECD Average";
+    x.overrideMin = -50;
+    x.overrideMax = 50;
+    var y = countryChart.addCategoryAxis("y", "Category");
+    y.addOrderRule("Order", true);
+    countryChart.addSeries(null, dimple.plot.bar);
+    countryChart.draw();
 
     countries.on("change", function(d){
-        var country = d3.select(this).property("value");
-        var x = countryChart.addMeasureAxis("x", country);
-        x.title = "Comparison with OECD Average";
-        x.overrideMin = -50;
-        x.overrideMax = 50;
-        var y = countryChart.addCategoryAxis("y", "Category");
-        y.addOrderRule("Order", true);
-        countryChart.addSeries(null, dimple.plot.bar);
-        countryChart.draw();
+        country = d3.select(this).property("value");
+        x.measure = country;
+        countryChart.draw(1000);
     });
 });
