58ae5259ba6a6b9b1d6125ba4cacd48f8e0616ed
mrmcnerd <blueark@outlook.com>
2017-05-24 07:11:39 +0100

    Sets default option to Denmark

--- js/graph.js
+++ js/graph.js
@@ -1,9 +1,10 @@
-var svg = dimple.newSvg("#chart", 1200, 600);
-var countries = d3.select("#countries");
-var country = "Denmark";
+"use strict";
 
 d3.tsv("all_countries.tsv", function (data) {
-    "use strict";
+    var svg = dimple.newSvg("#chart", 1200, 600);
+    var country = "Denmark";
+    SelectCountry(country);
+    var countries = d3.select("#countries");
     var countryChart = new dimple.chart(svg, data);
     countryChart.setMargins(200, 30, 50, 50);
     var x = countryChart.addMeasureAxis("x", country);
@@ -21,3 +22,13 @@ d3.tsv("all_countries.tsv", function (data) {
         countryChart.draw(1000);
     });
 });
+
+function SelectCountry(country) {
+    var countrySelect = document.getElementById("countries");
+    for (var i, j = 0; i = countrySelect.options[j]; j++) {
+        if(i.value == country) {
+            countrySelect.selectedIndex = j;
+            break;
+        }
+    }
+}
